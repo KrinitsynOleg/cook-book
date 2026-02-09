a8e80d31dd094a6042497ab96b9c5632ec065646
KrinitsyinOleg <olegkrinicyn1000@gmail.com>
2026-02-09 13:43:11 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+
+<template>
+
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+<button @click="test()">test</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+
+
+const count = ref(0)
+function test(){
+  console.log(count.value)
+}
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
